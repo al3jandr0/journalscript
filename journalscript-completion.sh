d779bcf7a65b18c8c357875ec8260c6077c50651
alej <nerdalej@gmail.com>
2021-09-26 22:48:24 -0400

    deletes old name convention of autocompletion script

--- journalscript-completion.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env bash
-function _homes() {
-    local journal_dirs=( "$HOME/repos/journal/$2"*/ )
-    local journal_names=( "${journal_dirs[@]%\/}" )
-    [[ -d ${journal_names[0]} ]] && COMPREPLY=( "${journal_names[@]##*/}" )
-}
-complete -F _journal journal
-
