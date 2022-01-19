22b6a05e07d97d8fe291783907e496b287bd5075
alej <nerdalej@gmail.com>
2022-01-19 16:42:22 -0500

    Fixes bug: worng completion function name

--- journal.bash
+++ journal.bash
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
 # bash-completion for journal command
-function _homes() {
+function _journal() {
     local journal_dirs=( "$HOME/repos/journal/$2"*/ )
     local journal_names=( "${journal_dirs[@]%\/}" )
     [[ -d ${journal_names[0]} ]] && COMPREPLY=( "${journal_names[@]##*/}" )
