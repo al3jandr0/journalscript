39a8b4ada71e157f63468d178c1a762976befc2a
alej <nerdalej@gmail.com>
2022-12-15 23:14:07 -0500

    Makes messaging of inig_config consistent

---
+++ init_configuration.sh
@@ -0,0 +1,64 @@
+#!/bin/env bash
+################################################################################
+# Provided variables                                                           #
+################################################################################
+# Ignores existing Env vars since the goal is to generate new configuration
+_JS_CONF_FILE_NAME="journalscript.env"
+_JS_CONF_DIR="$HOME/.config/journalscript"
+JS_CONF_FILE_TYPE="md"
+JS_CONF_EDITOR="typora"
+JS_CONF_DATA_DIR="$HOME/repos/journal"
+JS_CONF_TEMPLATE_DIR="$JS_CONF_DATA_DIR/.journalscript/templates"
+
+################################################################################
+# Read (prompt) configuration preferences from user                            #
+################################################################################
+read -p "Journal file format ($JS_CONF_FILE_TYPE):" p_file_type
+read -p "Editor ($JS_CONF_EDITOR):" p_editor
+read -p "Journal directory ($JS_CONF_DATA_DIR):" p_data_dir
+read -p "Journal templates directory ($JS_CONF_TEMPLATE_DIR):" p_tempalte_dir
+
+# Default values if no user input
+file_type=${p_file_type:-$JS_CONF_FILE_TYPE}
+editor=${p_editor:-$JS_CONF_EDITOR}
+data_dir=${p_pdata_dir:-$JS_CONF_DATA_DIR}
+template_dir=${p_tempalte_dir:-$JS_CONF_TEMPLATE_DIR}
+
+################################################################################
+# Validate                                                                     #
+################################################################################
+if ! command -v "$editor" > /dev/null 2>&1; then
+    echo "WARNING: could not find editor in system. Verify it is installed"
+fi
+if ! test -d "$data_dir"; then
+    echo "Journals directory $data_dir will be created"
+fi
+if ! test -d "$template_dir"; then
+    echo "Templates directory $data_dir will be created"
+fi
+if ! test -d "$_JS_CONF_DIR"; then
+    echo "Configuration directory $_JS_CONF_DIR will be created"
+fi
+if test -f "$_JS_CONF_DIR/$_JS_CONF_FILE_NAME"; then
+    echo "The configuration file $_JS_CONF_DIR/$_JS_CONF_FILE_NAME will be overriden."
+else
+    echo "A new configuration file $_JS_CONF_DIR/$_JS_CONF_FILE_NAME will be created"
+fi
+
+################################################################################
+# Execute: Write config directory and file                                     #
+################################################################################
+read -p "Conform changes? [y/n]:" confirm
+[[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 0
+
+mkdir -p "$data_dir"
+mkdir -p "$template_dir"
+mkdir -p "$_JS_CONF_DIR"
+
+# TODO:  write configuration to file
+cat <<-EOF
+JS_CONF_FILE_TYPE="$file_type"
+JS_CONF_EDITOR="$editor"
+JS_CONF_DATA_DIR="$data_dir"
+JS_CONF_TEMPLATE_DIR="$template_dir"
+EOF
--- journal.sh
+++ journal.sh
@@ -3,8 +3,12 @@
 # - bash
 # - git
 
+################################################################################
+# Parse Arguments                                                              #
+################################################################################
 COMMADN_LS="ls"
 COMMAND_WRITE="write"
+COMMAND_CONFIGURE="configure"
 SUB_COMAND_WRITE_EDIT="edit"
 SUB_COMMAND_WRITE_CREATE="create"
 
@@ -12,15 +16,19 @@ for i in "$@"; do
     case $i in
         --help)
 			cat <<-EOF
-			Usage: journal [CONFIG_JOURNAL_NAME]
+			Usage: journal [JS_CONF_JOURNAL_NAME]
 			A handy script to write journals.
-			    ls		displays journals stored in CONFIG_DATA_DIR 
-			    --help	displays this help and exit
+			    ls			displays available journals 
+                configure	initializes journalscript configuration
+			    --help		displays this help and exit
 			EOF
             exit 0
             ;;
         ls)
             COMMAND="$COMMAND_LS"
+            ;;
+        configure)
+            COMMAND="$COMMAND_CONFIGURE"
             ;;
         *)
             COMMAND="$COMMAND_WRITE"
@@ -30,33 +38,43 @@ for i in "$@"; do
     shift
 done
 
+################################################################################
+# Set configuration                                                            #
+################################################################################
+
 # Configuration defaults
-CONFIG_FILE_TYPE="md"
-CONFIG_EDITOR="nvim"
-# TODO:
-# This cant be the default....
-# You shoudl check when runnig the script!
-# Is there a dependency with install ?
-CONFIG_DATA_DIR="$HOME/repos/journal"
-CONFIG_TEMPLATE_DIR="$CONFIG_DATA_DIR/.journalscrript/templates"
+JS_CONF_FILE_TYPE="md"
+JS_CONF_EDITOR="nvim"
+JS_CONF_DATA_DIR="$HOME/repos/journal"
+JS_CONF_TEMPLATE_DIR="$JS_CONF_DATA_DIR/.journalscrript/templates"
 
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+# Returns the full path to the configuration directory
+config_dir() {
+    if test -d "$XDG_CONFIG_HOME/journalscript"; then
+        echo "$XDG_CONFIG_HOME/journalscript"
+    elif test -d "$HOME/.journalscript"; then
+        echo "$HOME/.journalscript"
+    fi
+}
 # Look for configuration in the following locations in order
 # 1. $XDG_CONFIG_HOME/journalscript/journalscript.env
 # 2. $HOME/.config/journalscript/journalscript.env
 # 3. $HOME/.journalscript/journalscript.env
-read_config() {
-    if test -d "$XDG_CONFIG_HOME/journalscript"; then
-        . $XDG_CONFIG_HOME/journalscript/journalscript.env
-    elif test -d "$HOME/.journalscript"; then
-        . $HOME/.journalscript/journalscript.env
-    fi
-}
+_JS_CONF_DIR=config_dir
+_JS_CONF_FILE_NAME="journalscript.env"
+. $_JS_CONF_DIR/$_JS_CONF_FILE_NAME.env
+# Re-set in case it is overriden by journalscript.env
+_JS_CONF_DIR=config_dir
+
+################################################################################
+# Set configuration                                                            #
+################################################################################
 
 write_template() {
     local journalName="$1"
     local journalEntryFile="$2"
-    local template="$CONFIG_TEMPLATE_DIR/$journalName"
+    local template="$JS_CONF_TEMPLATE_DIR/$journalName"
 
     if test -f "$template"; then
         while read line; do
@@ -76,38 +94,52 @@ backup_file() {
     else
         commit_msg="Edits entry"
     fi
-    git -C "$CONFIG_DATA_DIR" add "$1" && git -C "$CONFIG_DATA_DIR" commit -m "$commit_msg" && git -C "$CONFIG_DATA_DIR" push
+    git -C "$JS_CONF_DATA_DIR" add "$1" && git -C "$JS_CONF_DATA_DIR" commit -m "$commit_msg" && git -C "$JS_CONF_DATA_DIR" push
 }
 
 # TODO: Fix files to remove the directory in position 0
 open_files() {
     local files=("$@")
-    if [[ "$CONFIG_EDITOR" == *"vim" ]]; then
-        $CONFIG_EDITOR -o "${files[@]}"
+    if [[ "$JS_CONF_EDITOR" == *"vim" ]]; then
+        $JS_CONF_EDITOR -o "${files[@]}"
     else
-        $CONFIG_EDITOR "${files[0]}"
+        $JS_CONF_EDITOR "${files[0]}"
     fi
 }
 
 today=$(date +%Y-%m-%d)
-filename="$CONFIG_DATA_DIR/$JOURNAL/$today.$CONFIG_FILE_TYPE"
+filename="$JS_CONF_DATA_DIR/$JOURNAL/$today.$JS_CONF_FILE_TYPE"
 case $COMMAND in
     ls)
+        # Validate
+        # Execute
         # TODO: filter ignored files
-        ls "$CONFIG_DATA_DIR"
+        ls "$JS_CONF_DATA_DIR"
         exit 0
         ;;
     write)
-        if ! test -d "$CONFIG_DATA_DIR/$JOURNAL"; then
-            echo "Failed to create journal entry. Journal $JOURNAL does not exists in $CONFIG_DATA_DIR" 1>&2
+        # Validate
+        if ! test -d "$JS_CONF_DATA_DIR"; then
+            echo "WANR: Looks like you dont have a designated directory to store your journals."
+            echo "WANR: The location $JS_CONF_DATA_DIR does not exist in your system"
+            echo "WARN: Run 'journalscript configure' to setup a directory to store your journals"
             exit 1
+        elif ! test -d "$JS_CONF_DATA_DIR/$JOURNAL"; then
+            echo "WARN: No journal found with name $JOURNAL"
+            read -p "Would you like to create a new jurnal named $JOURNAL? (y/n):" yes_no
+            if yes_no; then
+                mkdir "$JS_CONF_DATA_DIR/$JOURNAL"
+            else
+                exit 0
+            fi
         fi
+        # Execute
         SUB_COMMAND="$SUB_COMMAND_EDIT"
         if ! test -f $filename; then
             SUB_COMMAND="$SUB_COMMAND_CREATE"
             write_template "$JOURNAL" "$filename"
         fi
-        readarray -t filesToOpen < <(ls -tA $CONFIG_DATA_DIR/$JOURNAL/* | head -n2) # 2 most recent files
+        readarray -t filesToOpen < <(ls -tA $JS_CONF_DATA_DIR/$JOURNAL/* | head -n2) # 2 most recent files
         # Enable a backup hook
         open_files "${filesToOpen[@]}" && backup_file "$filename" "$SUB_COMMAND"
         exit 0
