ce02efe794bae0bbd89fb9152dd065e762fe48f7
alej <nerdalej@gmail.com>
2023-04-11 22:18:23 -0400

    Adds step to release to extract latest from changelog

---
+++ cicd/extract_latest_release.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+declare -i f
+f=0
+REGEX="^## "
+while read -r l; do
+    [[ "$l" =~ $REGEX ]] && if [[ $f -eq 0 ]]; then f=1; else exit 0; fi
+    [[ $f -eq 1 ]] && printf "%s\n" "$l"
+done <"${1:-/dev/stdin}"
